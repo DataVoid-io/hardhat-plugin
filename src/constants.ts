4177029fe441667c87f55ed8f93ef2a8c6198585
Ivan Zhidkov <ivan@zhidkov.net>
2024-02-29 12:16:55 +0100

    Update Encoder API URL

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,3 @@
 export const PLUGIN_NAME = 'datavoid'
 export const DEFAULT_GASLIMIT = 1_500_000
-export const ENCODER_API_URL = 'https://deploy.gasmagic.com/api/encoder/compress'
+export const ENCODER_API_URL = 'https://api.datavoid.io/encoder/compress'
